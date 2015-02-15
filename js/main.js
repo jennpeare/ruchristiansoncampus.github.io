3683ea043e8880d03331bf4e3fbb6d91ec4a3166
Jenny Shi <jennpeare@gmail.com>
2015-02-15 00:27:32 -0500

    updates

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Smooth scrolling
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 80
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+  $('.flaticon-down139').click(function() {
+    var target = $('#activities');
+    $('html,body').animate({
+      scrollTop: target.offset().top - 80
+    }, 1000);
+  });
+});
