9c18305be3a0f5ec6224a9732ea1f48d554a93dd
Jenny Shi <jennpeare@gmail.com>
2015-03-18 00:40:09 -0400

    added landing page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 // Smooth scrolling
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var currentPath = location.pathname.replace(/^\//,'');
+    var thisPath = this.pathname.replace(/^\//,'');
+    if (currentPath == thisPath && location.hostname == this.hostname) {
       var target = $(this.hash);
+      console.log(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
